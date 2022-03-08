2840687439d4a27c2a1aa19db2ddfd0f423e1f8b
Jessica Olsen <jaolsen@Jessicas-MacBook-Pro.local>
2022-03-07 22:59:16 -0500

    Start of adding definitions for class

--- budget_app.py
+++ budget_app.py
@@ -1,7 +1,24 @@
-class Category:
+class Category():
 
+    def __init__(self, category_name) : 
+        self.category_name = category_name
+    
+    
+    ledger = list()
 
+    #Defining Deposit
+    def deposit(self, amount, description) : 
+        if bool(description) == False : 
+            description = ''
+        self.amount = amount
+        self.description = description
+        
 
-
+    ledger.append(amount)
 
 def create_spend_chart(categories):
+    pass
+
+
+food = Category("Food")
+food.deposit(1000, "initial deposit")
